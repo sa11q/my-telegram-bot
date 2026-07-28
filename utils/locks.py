6f70da2b8360480494b95ab14c9de4bc774bbc5a
sa11q <sagyndykidk@gmail.com>
2026-07-29 02:40:12 +0500

    Add AsyncFileLock class for asynchronous file locking

--- utils/locks.py
+++ utils/locks.py
@@ -1,1 +1,21 @@
+import asyncio
+
+class AsyncFileLock:
+    def __init__(self):
+        self._lock = asyncio.Lock()
+
+    async def acquire(self):
+        await self._lock.acquire()
+
+    def release(self):
+        self._lock.release()
+
+    async def __aenter__(self):
+        await self.acquire()
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        self.release()
+
+file_lock = AsyncFileLock()
 
